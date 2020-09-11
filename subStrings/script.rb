18f0be2218a98f8b81821210e34b8a589533736e
Nicholas <69174708+berubenic@users.noreply.github.com>
2020-09-11 18:19:55 -0400

    finsished subString project

---
+++ subStrings/script.rb
@@ -0,0 +1,19 @@
+require 'pry-byebug'
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(string, array)
+  string_to_array = string.downcase.split(/\W/)
+  matched_words = string_to_array.map do |string|
+    array.select { |word| string.include? word}
+  end
+  matched_words.flatten!
+  my_hash = {}
+  my_hash.default = 0
+  matched_words.each do |word|
+    my_hash[word] += 1    
+  end
+  p my_hash
+end
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
